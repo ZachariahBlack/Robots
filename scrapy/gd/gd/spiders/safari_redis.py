6c86bd56630fcfa003bc535b515a8a5e447d9274
Brant <xkdcc@users.noreply.github.com>
2018-12-01 21:11:58 -0800

    Create spiders to use scrapy_redis and test pass

---
+++ scrapy/gd/gd/spiders/safari_redis.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from pyquery import *
+from scrapy_redis.spiders import RedisSpider
+
+
+class SafariRedisSpider(RedisSpider):
+    name = 'safari-redis'
+    allowed_domains = ['oreilly.com']
+    # Get TOC of Book: Advanced Linux System Administration
+    # start_urls = ['https://www.oreilly.com/library/view/advanced-linux-system/9781789132748/']
+
+    def parse(self, response):
+        jpy = PyQuery(response.text)
+        chapters = jpy('.toc-level-1').items()
+        
+        for chapter in chapters:
+            chapter_title = chapter('a:first').text()
+
+            sub_chapters_titles = []
+            
+            for sub_chapter in chapter('.toc-level-2 > a'):
+                sub_chapters_titles.append(sub_chapter.text)
+            yield {chapter_title:sub_chapters_titles}
